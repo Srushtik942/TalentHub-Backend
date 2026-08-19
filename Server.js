75d096229d24627f53dbe3f756a6473f60f43906
srushtikulkarni <srushtikulkarni09@gmail.com>
2026-08-19 21:40:46 +0530

    Added healt check route

---
+++ Server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health",(req,res)=>{
+    res.status(200).send("Server is healthy");
+})
+
+
+app.listen((process.env.PORT), () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
